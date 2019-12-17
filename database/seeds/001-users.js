79418cc8218880e76ff314af5d0ace4ead2989ab
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-12-17 15:41:08 -0600

    deleted id in user seed

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -7,7 +7,6 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('users').insert([
         {
-          id: 1,
           password: bcrypt.hashSync('password123', 10),
           email: 'selena.gomez@sample.com'
         }
