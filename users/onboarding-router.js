f66b17154004a8ba55ecb846fc0ec23612e9244b
Joshua Sharp <sharp.josh@gmail.com>
2020-01-14 21:53:07 -0500

    Moved login logic to helper function in users-model
    to be shared/called in onboarding-router and users-router, also added logic for 3rd party auth

--- users/onboarding-router.js
+++ users/onboarding-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const Users = require("./users-model");
+const Vehicle = require('./vehicle-model');
 const generateToken = require("../auth/gen-token.js").generateToken;
 
 //add a user
@@ -12,9 +13,25 @@ router.post("/onboarding", (req, res) => {
   //add the user
   Users.add(user)
     .then(user => {
-      //res.status(201).json(user);
+      let {status, error} = Users.login(email, password) 
+      if (status = 200) {
+        res.status(200).json({
+          message: `Welcome ${user.email}!`,
+          token,
+          user
+        });
+        //Add vehicle
+        Vehicle.add(vehicle)
+        //Add Routing Prefs
+        //TODO: Extend BE with Route Faetures API/Endpoints
+      } else if (status = 401) {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      } else if (status = 500) {
+        res.status(500).json(error);
+      }
     })
     .catch(error => {
+      //Add user failed
       res.status(500).json(error);
     });
 });
