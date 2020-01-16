48b570706dfd0c1d8a7449eb7940c7ba28a685a7
DannyManzietti <danmanzietti@gmail.com>
2020-01-16 17:48:09 -0500

    updated oboarding and user and vehicle models

--- users/users-model.js
+++ users/users-model.js
@@ -6,13 +6,38 @@ module.exports = {
   findBy,
   findUsers,
   removeUser,
-  login
+  login,
+  updateUserFirstName,
+  updateUserLastName,
+  updateUserName,
+  updateAge
 };
 
 async function add(user) {
   const [id] = await db("users").insert(user, "id");
 
   return findById(id);
+}
+
+function updateUserFirstName(id, firstname) {
+  return db("users")
+    .where({ id })
+    .update(firstname);
+}
+function updateUserLastName(id, lastname) {
+  return db("users")
+    .where({ id })
+    .update(lastname);
+}
+function updateUserName(id, username) {
+  return db("users")
+    .where({ id })
+    .update(username);
+}
+function updateAge(id, age) {
+  return db("users")
+    .where({ id })
+    .update(age);
 }
 
 function findById(id) {
@@ -24,13 +49,13 @@ function findById(id) {
 function findBy(filter) {
   return db("users").where(filter);
 }
-function findByUser(user) {
-  return db("users").where(function() {
-    this.where('email', user.email)
-    .orWhere('googleEmail', user.googleEmail)
-    .orWhere('facebookEmail', user.facebookEmail);
-  });
-}
+// function findByUser(user) {
+//   return db("users").where(function() {
+//     this.where('email', user.email)
+//     .orWhere('googleEmail', user.googleEmail)
+//     .orWhere('facebookEmail', user.facebookEmail);
+//   });
+// }
 
 function findUsers() {
   return db("users");
@@ -41,25 +66,26 @@ function removeUser(id) {
     .where({ id })
     .del();
 }
-function login(user) {
-  Users.findByUser({ user })
-  .first()
-  .then(foundUser => {
-    if (foundUser && 
-          (bcrypt.compareSync(password, user.password) ||
-          bcrypt.compareSync(foundUser.password, user.googleId) ||
-          bcrypt.compareSync(foundUser.password, user.password)
-        ))
-    {
-      const token = generateToken(user);
-      //Return login success
-      return 200;
-    } else {
-      //Return invalid credentials
-      return 401;
-    }
-  })
-  .catch(error => {
-    return 500, error;
-  });
-}
+
+// function login(user) {
+//   Users.findByUser({ user })
+//   .first()
+//   .then(foundUser => {
+//     if (foundUser &&
+//           // (bcrypt.compareSync(password, user.password) ||
+//           // bcrypt.compareSync(foundUser.password, user.googleId) ||
+//           (bcrypt.compareSync(foundUser.password, user.password)
+//         ))
+//     {
+//       const token = generateToken(user);
+//       //Return login success
+//       return 200;
+//     } else {
+//       //Return invalid credentials
+//       return 401;
+//     }
+//   })
+//   .catch(error => {
+//     return 500, error;
+//   });
+// }
--- vehicles/vehicle-model.js
+++ vehicles/vehicle-model.js
@@ -1,4 +1,4 @@
-const db = require('../database/dbconfig.js');
+const db = require("../database/dbconfig.js");
 
 module.exports = {
   add,
@@ -6,37 +6,105 @@ module.exports = {
   findById,
   findBy,
   findUsersVehicles,
-  updateVehicle
+  updateVehicle,
+  addVehicleName,
+  addVehicleHeight,
+  addVehicleAxel,
+  addVehicleClass,
+  addVehicleTires,
+  addVehicleTrailer,
+  addVehicleWidth,
+  addVehicleDirtRoads,
+  addVehiclePotholes,
+  addVehicleSteepGrade,
+  addVehicleLength,
+  addVehicleWeight
 };
 
 async function add(vehicle) {
-  const [id] = await db('vehicle').insert(vehicle, 'id');
+  const [id] = await db("vehicle").insert(vehicle, "id");
 
   return findById(id);
 }
 
-function findById(id) {
-  return db('vehicle')
+function addVehicleName(id, name) {
+  return db("vehicle")
     .where({ id })
-    .first();
+    .insert(name);
+}
+function addVehicleHeight(id, height) {
+  return db("vehicle")
+    .where({ id })
+    .insert(height);
+}
+function addVehicleWidth(id, width) {
+  return db("vehicle")
+    .where({ id })
+    .insert(width);
+}
+function addVehicleLength(id, length) {
+  return db("vehicle")
+    .where({ id })
+    .insert(length);
+}
+function addVehicleWeight(id, weight) {
+  return db("vehicle")
+    .where({ id })
+    .insert(weight);
+}
+function addVehicleAxel(id, axel_count) {
+  return db("vehicle")
+    .where({ id })
+    .insert(axel_count);
+}
+function addVehicleClass(id, vehicle_class) {
+  return db("vehicle")
+    .where({ id })
+    .insert(vehicle_class);
+}
+function addVehicleTires(id, dual_tires) {
+  return db("vehicle")
+    .where({ id })
+    .insert(dual_tires);
+}
+
+function addVehicleTrailer(id, trailer) {
+  return db("vehicle")
+    .where({ id })
+    .insert(trailer);
+}
+function addVehicleDirtRoads(id, DirtRoads) {
+  return db("vehicle")
+    .where({ id })
+    .insert(DirtRoads);
+}
+function addVehicleSteepGrade(id, SteepGrade) {
+  return db("vehicle")
+    .where({ id })
+    .insert(SteepGrade);
+}
+function addVehiclePotholes(id, Potholes) {
+  return db("vehicle")
+    .where({ id })
+    .insert(Potholes);
 }
 
 function findBy(filter) {
-  return db('vehicle').where(filter);
+  return db("vehicle").where(filter);
 }
 
 function findUsersVehicles(user_id) {
-  return db('vehicle').where({ user_id });
+  return db("vehicle").where({ user_id });
 }
 
 function updateVehicle(id, changedVehicle) {
-  return db('vehicle')
+  return db("vehicle")
     .where({ id })
     .update(changedVehicle);
 }
 
 function deleteVehicle(id) {
-  return db('vehicle')
+  return db("vehicle")
     .where({ id })
     .del();
 }
