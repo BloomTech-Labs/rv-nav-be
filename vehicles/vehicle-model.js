9991e7f405c66be821a392ba6bce8fa04c0c86da
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-02-03 12:20:27 -0500

    clean up! consolelogs and commented out code

--- vehicles/vehicle-model.js
+++ vehicles/vehicle-model.js
@@ -10,11 +10,8 @@ module.exports = {
 };
 
 async function add(vehicle) {
-  console.log("Made it!");
   const response = await db("vehicle").insert(vehicle);
-  console.log("add response", vehicle);
   return vehicle;
-  // return db("vehicle").insert(vehicle);
 }
 
 function findBy(filter) {
@@ -26,7 +23,6 @@ function findById(id) {
 }
 
 async function findUsersVehicles(user_id) {
-  console.log("u here bro?", user_id);
   const cars = await db("vehicle").where({ user_id: user_id });
   return cars;
 }
